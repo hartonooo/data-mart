f14ad13ce327779e28a0d0eb0d7000ddef7452de
Hartono <124220894+mas-tono@users.noreply.github.com>
2023-02-26 16:39:36 +0700

    Update data_mart.sql

--- data_mart.sql
+++ data_mart.sql
@@ -1,5 +1,5 @@
 --1. DATA CLEANING
-select *,
+select region, platform, segment, customer_type, transactions, sales,
 SUBSTRING(week_date,2, CHARINDEX('/', week_date, 1)-2)  as day, 
 SUBSTRING(week_date, CHARINDEX('/', week_date, 1)+1, 1) as month_number,
 concat('20', SUBSTRING(week_date, CHARINDEX('/', week_date, CHARINDEX('/', week_date)+1)+1, 2)) as calendar_year,
@@ -27,11 +27,13 @@ round(sales*100.0/transactions, 2) as avg_transaction
 into clean_weekly_sales
 from data_mart_weekly_sales;
 
+select *
+from clean_weekly_sales
 
 --2. DATA EXPLORATION
 
 --1. What day of the week is used for each week_date value?
-select distinct date_full, DAY(date_full)
+select distinct date_full, DAY(date_full) as day_used
 from clean_weekly_sales
 order by date_full;
 
@@ -51,12 +53,26 @@ from clean_weekly_sales
 group by calendar_year;
 
 
-
 --4. What is the total sales for each region for each month?
-select region, sum(CAST(sales as bigint)) as total_sales
+select region, month_number, sum(CAST(sales as bigint)) as total_sales
 from clean_weekly_sales
-group by region;
+group by region, month_number
+order by region, month_number;
 
+-------------------------------------------------
+--pivot
+
+with satu as (select *
+from (select region, month_number, CAST(sales as bigint) as sales
+	from clean_weekly_sales) s
+pivot(
+	sum(sales)
+	for region in (['AFRICA'], ['ASIA'], ['CANADA'], ['EUROPE'], ['OCEANIA'], ['SOUTH AMERICA'], ['USA'])
+) pivot_table)
+
+select *
+from satu
+order by month_number;
 
 
 --5. What is the total count of transactions for each platform
@@ -68,33 +84,44 @@ group by platform;
 --6. What is the percentage of sales for Retail vs Shopify for each month?
 with satu as (select calendar_year, month_number, platform, sum(CAST(sales as bigint)) as total_sales
 from clean_weekly_sales
-group by calendar_year, month_number
-)
+group by calendar_year, month_number, platform
+), 
 
-select *, 
+dua as (select *, 
 SUM(total_sales) over(partition by calendar_year) as total_sales_all, 
 round(total_sales * 100.0 / SUM(total_sales) over(partition by calendar_year, month_number), 2) as pct_sales_per_month
-from satu
-order by calendar_year, month_number, platform;
+from satu)
+
+select *
+from (select calendar_year, platform, month_number, pct_sales_per_month from dua) s
+pivot (
+	max(pct_sales_per_month)
+	for calendar_year in ([2018], [2019], [2020])
+) pvt
 
 
 --7. What is the percentage of sales by demographic for each year in the dataset?
 with satu as (select calendar_year, demographic, sum(CAST(sales as bigint)) as total_sales
 from clean_weekly_sales
-group by calendar_year, demographic)
+group by calendar_year, demographic),
 
-select *, SUM(total_sales) over(partition by calendar_year) as total_sales_all,
+dua as (select *, SUM(total_sales) over(partition by calendar_year) as total_sales_all,
 round(total_sales * 100.0 / SUM(total_sales) over(partition by calendar_year), 2) as pct_demographic_per_year
-from satu
-order by calendar_year, demographic;
+from satu)
+
+select *
+from (select demographic, calendar_year, pct_demographic_per_year from dua) s
+pivot(
+	max(pct_demographic_per_year)
+	for calendar_year in ([2018], [2019], [2020])
+) pvt
 
 
 --8. Which age_band and demographic values contribute the most to Retail sales? 
-select age_band, demographic, sum(CAST(sales as bigint)) as total_sales
+select platform, age_band, demographic, sum(CAST(sales as bigint)) as total_sales, RANK() over(order by sum(CAST(sales as bigint)) desc) as ranking
 from clean_weekly_sales
-where platform = '''Retail'''
-group by age_band, demographic
-order by sum(CAST(sales as bigint)) desc;
+where platform = '''Retail''' and age_band <> 'unknown'
+group by platform, age_band, demographic;
 
 --> unknown, retirees families or retirees couples
 
@@ -124,7 +151,6 @@ select sum(CAST(sales as bigint)) as total_sales_before
 from satu
 where date_full between DATEADD(week, -4, '2020-06-15') and '2020-06-15'),
 
-
 total_sales_4_weeks_after as(
 select sum(CAST(sales as bigint)) as total_sales_after
 from satu
@@ -135,7 +161,7 @@ case
 	when total_sales_4_weeks_before.total_sales_before > total_sales_4_weeks_after.total_sales_after then 'before is more trx' else 'after is more trx'
 end as status,
 total_sales_4_weeks_before.total_sales_before - total_sales_4_weeks_after.total_sales_after as diff
-from total_sales_4_weeks_before, total_sales_4_weeks_after
+from total_sales_4_weeks_before, total_sales_4_weeks_after;
 --> more trx before new program launched, in timeline 4 weeks before and 4 weeks after
 --> the difference: 10.973.134
 
@@ -167,20 +193,21 @@ total_sales_12_weeks_before.total_sales_before - total_sales_12_weeks_after.tota
 from total_sales_12_weeks_before, total_sales_12_weeks_after
 
 --> more trx before new program launched, in timeline 12 weeks before and 12 weeks after
---> the difference got more bigger for 'before' status: 722.350.742
+--> the difference with highest negative  bigger for 'before' status: 722.350.742
 
 
 
 --3. How do the sale metrics for these 2 periods before and after compare with the previous years in 2018 and 2019?
 select calendar_year, sum(CAST(sales as bigint)) as total_sales
 from clean_weekly_sales
-group by calendar_year
+where calendar_year in (2018, 2019)
+group by calendar_year;
 
 --> with total_sales before new program:
 	--2018 around 13 million, 
 	--2019 around 14 million,
 	--4 weeks before and
-	--12 weeks after
+	--12 weeks before
 --all show bigger total_sales compared to total_sales after new program launched
 
 
@@ -214,13 +241,13 @@ select b.region,
 		b. total_sales_before, 
 		a.total_sales_after,
 		(b.total_sales_before - a.total_sales_after) as diff,
-		round((b.total_sales_before - a.total_sales_after) *100.0 / b. total_sales_before, 2) as pct
+		round((b.total_sales_before - a.total_sales_after) *100.0 / b. total_sales_before, 2) as pct_impact
 from total_sales_12_weeks_before b
 join total_sales_12_weeks_after a
 on b.region = a.region
 order by (b.total_sales_before - a.total_sales_after) *100.0 / b. total_sales_before desc;
 
---> the region got more impact is asia
+--> the region with highest negative impact is asia
 
 
 --2. PLATFORM:
@@ -248,13 +275,13 @@ select b.platform,
 		b. total_sales_before, 
 		a.total_sales_after,
 		(b.total_sales_before - a.total_sales_after) as diff,
-		round((b.total_sales_before - a.total_sales_after) *100.0/ b. total_sales_before, 2) as pct
+		round((b.total_sales_before - a.total_sales_after) *100.0/ b. total_sales_before, 2) as pct_impact
 from total_sales_12_weeks_before b
 join total_sales_12_weeks_after a
 on b.platform = a.platform
 order by round((b.total_sales_before - a.total_sales_after) *100.0/ b. total_sales_before, 2) desc;
 
---> the platform got more impact is retail
+--> the platform with highest negative impact is retail
 
 
 
@@ -272,7 +299,6 @@ from satu
 where date_full between DATEADD(week, -12, '2020-06-15') and '2020-06-15'
 group by age_band),
 
-
 total_sales_12_weeks_after as(
 select age_band, sum(CAST(sales as bigint)) as total_sales_after
 from satu
@@ -283,13 +309,14 @@ select b.age_band,
 		b. total_sales_before, 
 		a.total_sales_after,
 		(b.total_sales_before - a.total_sales_after) as diff,
-		round((b.total_sales_before - a.total_sales_after) *100.0/ b. total_sales_before, 2) as pct
+		round((b.total_sales_before - a.total_sales_after) *100.0/ b. total_sales_before, 2) as pct_impact
 from total_sales_12_weeks_before b
 join total_sales_12_weeks_after a
 on b.age_band = a.age_band
+where b.age_band <> 'unknown'
 order by round((b.total_sales_before - a.total_sales_after) *100.0/ b. total_sales_before, 2) desc;
 
---> the age_band got more impact is middle aged
+--> the age_band with highest negative impact is middle aged
 
 
 --4. demographic:
@@ -306,7 +333,6 @@ from satu
 where date_full between DATEADD(week, -12, '2020-06-15') and '2020-06-15'
 group by demographic),
 
-
 total_sales_12_weeks_after as(
 select demographic, sum(CAST(sales as bigint)) as total_sales_after
 from satu
@@ -317,13 +343,14 @@ select b.demographic,
 		b. total_sales_before, 
 		a.total_sales_after,
 		(b.total_sales_before - a.total_sales_after) as diff,
-		round((b.total_sales_before - a.total_sales_after) *100.0/ b. total_sales_before, 2) as pct
+		round((b.total_sales_before - a.total_sales_after) *100.0/ b. total_sales_before, 2) as pct_impact
 from total_sales_12_weeks_before b
 join total_sales_12_weeks_after a
 on b.demographic = a.demographic
+where b.demographic <> 'unknown'
 order by round((b.total_sales_before - a.total_sales_after) *100.0/ b. total_sales_before, 2) desc;
 
---> the demographic got more impact is families
+--> the demographic with highest negative  impact is families
 
 --5 customer type:
 
@@ -339,7 +366,6 @@ from satu
 where date_full between DATEADD(week, -12, '2020-06-15') and '2020-06-15'
 group by customer_type),
 
-
 total_sales_12_weeks_after as(
 select customer_type, sum(CAST(sales as bigint)) as total_sales_after
 from satu
@@ -350,12 +376,12 @@ select b.customer_type,
 		b. total_sales_before, 
 		a.total_sales_after,
 		(b.total_sales_before - a.total_sales_after) as diff,
-		round((b.total_sales_before - a.total_sales_after) *100.0/ b. total_sales_before, 2) as pct
+		round((b.total_sales_before - a.total_sales_after) *100.0/ b. total_sales_before, 2) as pct_impact
 from total_sales_12_weeks_before b
 join total_sales_12_weeks_after a
 on b.customer_type = a.customer_type
 order by round((b.total_sales_before - a.total_sales_after) *100.0/ b. total_sales_before, 2) desc;
 
---> the customer type got more impact is guest
+--> the customer type with highest negative  impact is guest
 
 
